589f7d35e1781195476f65c9138ffa10e789aab2
TahaBerk <tahaberkkucuk@gmail.com>
2024-08-14 21:59:22 +0300

    minor fixes

--- api_app/core/views.py
+++ api_app/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponse
 import json
@@ -26,24 +26,29 @@ def webhook(request):
 
     return JsonResponse({"status": "bad request"}, status=400)
 
-def send_text(request, to, text):
-    api = "EAAV3yOYZCYRsBOy9zWIZBZAEefLud2ZAMT1DRL3xEvNgfurldoYiNnPy64vwjam2iOrSJtoNNZBZCv4JYbENVzlip6OTt23XlrhCgZCUoPtUd9qHxudLwIGdjwDoYF8G6GnLcQWZCNE5BAmE1VNvuC4VyKy0znbuPhNZBnpVIUBpddSWKTKUZCDtZBPIFc5dkDOLLvYq9eiyhAiXOcTecg1Bo0ZD"
-    phone_id = "367538839783725"
-    url = f"https://graph.facebook.com/v20.0/{phone_id}/messages"
+def send_text(request):
+    if request.method == "POST":
+        api = "EAAV3yOYZCYRsBOy9zWIZBZAEefLud2ZAMT1DRL3xEvNgfurldoYiNnPy64vwjam2iOrSJtoNNZBZCv4JYbENVzlip6OTt23XlrhCgZCUoPtUd9qHxudLwIGdjwDoYF8G6GnLcQWZCNE5BAmE1VNvuC4VyKy0znbuPhNZBnpVIUBpddSWKTKUZCDtZBPIFc5dkDOLLvYq9eiyhAiXOcTecg1Bo0ZD"
+        phone_id = "367538839783725"
+        url = f"https://graph.facebook.com/v20.0/{phone_id}/messages"
+        
+        to = request.POST.get("to")
+        text = request.POST.get("text")
 
-    head ={
-        "Authorization": f"Bearer {api}",
-        "Content-Type": "application/json",
-    }
-    data = {
-        "messaging_product": "whatsapp",
-        "to": to,
-        "type": "text",
-        "text": {
-            "body": text
+        header ={
+            "Authorization": f"Bearer {api}",
+            "Content-Type": "application/json",
         }
-    }
-    response = requests.post(url, headers=head, data=json.dumps(data))
-    return response.json()
+        data = {
+            "messaging_product": "whatsapp",
+            "to": to,
+            "type": "text",
+            "text": {
+                "body": text
+            }
+        }
+        response = requests.post(url, headers=header, data=json.dumps(data))
+                
+    return render(request, "send_page.html")
 
 # Create your views here.
